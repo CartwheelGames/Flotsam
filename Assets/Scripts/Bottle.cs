6c404ec6804560a57cddf81564b4f9b085e34c9c
Peter <petermsmith@gmail.com>
2017-01-20 22:54:30 -0500

    Basic wave behavior

---
+++ Assets/Scripts/Bottle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bottle : MonoBehaviour {
+
+    Wave wave;
+
+	// Use this for initialization
+	void Start () {
+        wave = GameObject.Find("wave").GetComponent<Wave>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(0, wave.GetHeightAtXPos(0));
+
+	}
+}
---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wave : MonoBehaviour {
+
+    float scrollPos;
+    Renderer rend;
+    float scrollVel;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+        scrollVel = 5;
+    }
+
+    public float GetHeightAtXPos(float xPos)
+    {
+        return Mathf.Sin((((xPos + (scrollPos - 5)) / 5) * Mathf.PI) + Mathf.PI) * 2.5f;
+    }
+
+    public float GetSlopeAtXPos(float xPos)
+    {
+        return 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        scrollPos += Time.deltaTime * scrollVel;
+        scrollPos = scrollPos % 10;
+        transform.position = new Vector2(scrollPos, 0);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            scrollVel = -scrollVel;
+        }
+	}
+}
