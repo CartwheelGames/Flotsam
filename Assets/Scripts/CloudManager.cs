26ac16bb02e711179c43c78860d881cf9dd56385
Michael Consoli <michael@cartwheelgames.com>
2017-01-21 22:19:26 -0500

    Moving clouds

--- Assets/Scripts/CloudManager.cs
+++ Assets/Scripts/CloudManager.cs
@@ -5,6 +5,26 @@ namespace Project
 	public class CloudManager : MonoBehaviour
 	{
 		[SerializeField]
-		private Transform[] clouds;
+		private float minStartX = 2f;
+		[SerializeField]
+		private float maxStartX = 3f;
+		[SerializeField]
+		private float cloudSpeed = 0.5f;
+		[SerializeField]
+		private SpriteRenderer[] clouds;
+		private void Update()
+		{
+			foreach (SpriteRenderer cloud in clouds)
+			{
+				if (cloud.transform.position.x < -minStartX && !cloud.isVisible)
+				{
+					cloud.transform.position = new Vector3(Random.Range(minStartX, maxStartX), cloud.transform.position.y, 0f);
+				}
+				else
+				{
+					cloud.transform.Translate(Vector3.left * cloudSpeed * Time.deltaTime);
+				}
+			}
+		}
 	}
 }
